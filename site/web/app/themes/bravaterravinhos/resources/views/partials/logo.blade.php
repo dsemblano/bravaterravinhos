d4c1e809f5e9cd41fc2ad699cdc802c27ce7b0e6
Daniel Semblano <dsemblano@gmail.com>
2026-03-16 10:03:41 -0300

    Header position absolute, logo self centered

--- site/web/app/themes/bravaterravinhos/resources/views/partials/logo.blade.php
+++ site/web/app/themes/bravaterravinhos/resources/views/partials/logo.blade.php
@@ -1,4 +1,5 @@
-<a class="brand block max-w-[200px] w-full md:mb-4" href="{{ home_url('/') }}">
+<a class="brand block max-w-[500px] w-full md:mt-10 flex md:flex-row self-center
+" href="{{ home_url('/') }}">
   <div class="aspect-[200/56] w-full">
     <img width="200" height="56" class="w-full h-full object-contain block hover:scale-105 transition duration-300 ease-in-out" id="logo" alt="Logo da Néctar da Amazônia" src="{{ Vite::asset('resources/images/bravaterra_logo.webp') }}" />
   </div>
