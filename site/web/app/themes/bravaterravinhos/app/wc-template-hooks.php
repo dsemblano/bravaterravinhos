98ad8a74ccb77a0a3198c90ba4a753a513695842
Daniel Semblano <dsemblano@gmail.com>
2026-03-24 20:01:32 -0300

    related products duplicated fixed, composer update plugins

--- site/web/app/themes/bravaterravinhos/app/wc-template-hooks.php
+++ site/web/app/themes/bravaterravinhos/app/wc-template-hooks.php
@@ -129,8 +129,8 @@ remove_action('woocommerce_before_main_content', 'woocommerce_breadcrumb', 20);
  * @see woocommerce_output_related_products()
  */
 // remove_action('woocommerce_after_single_product_summary', 'woocommerce_output_product_data_tabs', 10);
-// remove_action('woocommerce_after_single_product_summary', 'woocommerce_upsell_display', 15);
-// remove_action('woocommerce_after_single_product_summary', 'woocommerce_output_related_products', 20);
+remove_action('woocommerce_after_single_product_summary', 'woocommerce_upsell_display', 15);
+remove_action('woocommerce_after_single_product_summary', 'woocommerce_output_related_products', 20);
 
 /**
  * Product Summary Box.
