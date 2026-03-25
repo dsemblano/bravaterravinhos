36b1ab7a7e2747d6f750020ea6ab45157bad84f0
Daniel Semblano <dsemblano@gmail.com>
2026-03-25 18:42:11 -0300

    CTA product, price and heading colors

--- site/web/app/themes/bravaterravinhos/resources/views/components/product-card.blade.php
+++ site/web/app/themes/bravaterravinhos/resources/views/components/product-card.blade.php
@@ -30,7 +30,7 @@
         </div>
 
         {{-- TITLE --}}
-        <h3 class="text-lg font-semibold leading-tight text-melescuro">
+        <h3 class="text-lg font-semibold leading-tight text-tertiary">
             <a href="{{ get_permalink($product->get_id()) }}">
                 {{ $product->get_name() }}
             </a>
@@ -45,7 +45,7 @@
 
         {{-- PRICE + CTA --}}
         <div class="flex items-center justify-between pt-3">
-            <span class="text-xl font-bold text-melescuro">
+            <span class="text-xl font-bold text-tertiary">
                 {!! $product->get_price_html() !!}
             </span>
 
