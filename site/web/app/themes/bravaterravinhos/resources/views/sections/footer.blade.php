47b01fa13bbb7feca0d26374282a550189735e5a
Daniel Semblano <dsemblano@gmail.com>
2026-03-31 15:28:55 -0300

    Footer updated, single product

--- site/web/app/themes/bravaterravinhos/resources/views/sections/footer.blade.php
+++ site/web/app/themes/bravaterravinhos/resources/views/sections/footer.blade.php
@@ -1,10 +1,10 @@
-<footer class="content-info bg-verde text-white py-6">
+<footer class="content-info bg-verde text-primary py-6">
     <div
-        class="container max-w-none text-white prose:a:text-white">
+        class="container max-w-none ">
         @php(dynamic_sidebar('sidebar-footer'))
-        <div class="text-sm mt-4 flex flex-col items-center copyright border-gray-500 border-t border-solid pt-8 gap-2 text-white">
-            <span class="z-10 font-bold text-white">© 2009 - {{ date('Y') }} 
-                <a class="text-white hover:underline" href="{{ home_url('/') }}">Brava Terra Vinhos</a>
+        <div class="text-sm mt-4 flex flex-col items-center copyright border-gray-500 border-t border-solid pt-8 gap-2">
+            <span class="z-10 font-bold ">{{ date('Y') }} 
+                <a class="hover:underline" href="{{ home_url('/') }}">Brava Terra Vinhos</a>
                 <span id="trademark" class="sup align-text-bottom">&reg;</span>
             </span>
         </div>
