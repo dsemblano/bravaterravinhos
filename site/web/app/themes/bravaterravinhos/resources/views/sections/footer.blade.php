eba7b1c8d6c75dd18d4c436d42a38cd4d89aee11
Daniel Semblano <dsemblano@gmail.com>
2026-03-31 15:19:41 -0300

    Footer updated

--- site/web/app/themes/bravaterravinhos/resources/views/sections/footer.blade.php
+++ site/web/app/themes/bravaterravinhos/resources/views/sections/footer.blade.php
@@ -4,7 +4,7 @@
         @php(dynamic_sidebar('sidebar-footer'))
         <div class="text-sm mt-4 flex flex-col items-center copyright border-gray-500 border-t border-solid pt-8 gap-2 text-white">
             <span class="z-10 font-bold text-white">© 2009 - {{ date('Y') }} 
-                <a class="text-white hover:underline" href="{{ home_url('/') }}">Néctar da Amazônia</a>
+                <a class="text-white hover:underline" href="{{ home_url('/') }}">Brava Terra Vinhos</a>
                 <span id="trademark" class="sup align-text-bottom">&reg;</span>
             </span>
         </div>
